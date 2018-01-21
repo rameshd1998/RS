77cfc252b773cd33eef8e6ea75020d0115e575a4
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-21 09:38:58 +0530

    nk.c

---
+++ nk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int c,d,a[40],i;
+int sum=0;
+printf("\n enter the number");
+scanf("%d%d",&c,&d);
+for(i=0;i<d;i++)
+{
+scanf("%d",&j[i]);
+}
+for(i=0;i<c;i++)
+{
+sum=sum+j[i];
+}
+printf("%d",sum);
+}
